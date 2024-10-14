aaff0ef12e1c460f8f48c656265646d298e277d0
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-14 15:54:44 +0200

    add link to script.js file and add variable numeroKm

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@
 const percentualeScontoU18 = 20;
 //dichiarare costante percentuale sconto o-65
 const percentualeScontoO65 = 40;
-//chidere a utente numero km con alert
+//chidere a utente numero km con prompt
 //salvarlo in una variabile
-//chidere a utente eta con alert
+let numeroKm = parseInt(prompt("inserisci il numero di km percorsi"));
+console.log(typeof(numeroKm))
+//chidere a utente eta con prompt
 //salvarlo in una variabile
 
 //calcolare prezzo base biglietto: km * costante 0,21
