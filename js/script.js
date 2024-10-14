4468cc64a813fe8f59c03c8e2a5789706b72e714
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-14 17:03:35 +0200

    made all the control to aplly the right discount

--- js/script.js
+++ js/script.js
@@ -6,31 +6,34 @@ const percentualeScontoO65 = 40;
 const prezzoPerKm = 0.21;
 //chidere a utente numero km con prompt
 //salvarlo in una variabile
-let numeroKm = prompt("inserisci il numero di km percorsi");
-console.log(numeroKm);
-let numeriDopoVirgola = numeroKm.slice(numeroKm.indexOf(",")+1);
-console.log(numeriDopoVirgola);
+let numeroKm = parseInt(prompt("inserisci il numero di km percorsi"));
 //chidere a utente eta con prompt
 //salvarlo in una variabile
 let eta = parseInt(prompt("inserisci la tua eta"));
-console.log(eta);
-console.log(typeof(eta));
-//SE 
 //calcolare prezzo base biglietto: km * costante 0,21
 let prezzoBase = numeroKm * prezzoPerKm;
-console.log(prezzoBase);
-
-let sconto;
+let prezzoFinale = 0;
+//inizzializzo la variabile sconto
+let sconto = 0; 
+console.log(sconto);
 //SE eta < 18 
     // sconto = prezzo * percentuale u-18 / 100
 //ALTRIMENTI se eta > 65 
     // sconto = prezzo * percentuale o-65 / 100
 if(eta<18){
-    sconto = prezzo * percentualeScontoU18 / 100
+    sconto = prezzoBase * percentualeScontoU18 / 100
+    console.log(sconto);
 }else if(eta > 65){
-    sconto = prezzo * percentualeScontoO65 / 100
+    sconto = prezzoBase * percentualeScontoO65 / 100
+    console.log(sconto);
 }
-//SE sconto != 0
-    //calcolo prezzo finale prezzo-sconto
+
+if(sconto != 0){
+    prezzoFinale = prezzoBase - sconto;
+}else{
+    prezzoFinale = prezzoBase;
+}
+
+alert(prezzoFinale.toFixed(2));
 
 //prezzo finale
