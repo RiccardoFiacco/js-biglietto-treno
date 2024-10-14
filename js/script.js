4935219deb92420c9d57da9206b8f373118d921c
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-14 15:59:13 +0200

    add var prezzoBase

--- js/script.js
+++ js/script.js
@@ -2,15 +2,21 @@
 const percentualeScontoU18 = 20;
 //dichiarare costante percentuale sconto o-65
 const percentualeScontoO65 = 40;
+//dichiare una costante con il prezzo per km
+const prezzoPerKm = 0.21;
 //chidere a utente numero km con prompt
 //salvarlo in una variabile
 let numeroKm = parseInt(prompt("inserisci il numero di km percorsi"));
-console.log(typeof(numeroKm))
+console.log(numeroKm);
+console.log(typeof(numeroKm));
 //chidere a utente eta con prompt
 //salvarlo in una variabile
-
+let eta = parseInt(prompt("inserisci la tua eta"));
+console.log(eta);
+console.log(typeof(eta));
 //calcolare prezzo base biglietto: km * costante 0,21
-
+let prezzoBase = numeroKm * prezzoPerKm;
+console.log(prezzoBase);
 //SE eta < 18 
     // sconto = prezzo * percentuale u-18 / 100
 //ALTRIMENTI se eta > 65 
