bd6e9934f323464c00a3b47a065f4308c871d34d
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-10-14 15:47:45 +0200

    step to do

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//dichiare costante percentuale sconto u-18
+//dichiarare costante percentuale sconto o-65
+//chidere a utente numero km con alert
+//salvarlo in una variabile
+//chidere a utente eta con alert
+//salvarlo in una variabile
+
+//calcolare prezzo base biglietto: km * costante 0,21
+
+//SE eta < 18 
+    // sconto = prezzo * percentuale u-18 / 100
+//ALTRIMENTI se eta > 65 
+    // sconto = prezzo * percentuale o-65 / 100
+
+//SE sconto != 0
+    //calcolo prezzo finale prezzo-sconto
+
+//prezzo finale
